440be52f3c959c35cc3c41371cb7aaa9b6f325ee
shmoss <shmoss@wisc.edu>
2019-05-29 21:08:20 -0700

    strptime bug fixed
    %b changed to %B to reflect full month (not abbreviated)

--- BandsintownWebScraper.py
+++ BandsintownWebScraper.py
@@ -17,12 +17,14 @@ import datetime
 from datetime import datetime as dt
 import re
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
+#Set driver options
 options = Options()
 options.add_argument('--no-sandbox') # Bypass OS security model
 driverLocation = webdriver.Chrome(chrome_options=options, executable_path=r'/Applications/chromedriver74')
-driverLocation.get('http://www.python.org')
-print(driverLocation.title)
+#driverLocation.get('http://www.python.org')
+#print(driverLocation.title)
 driverLocation.quit()
 
 def suffix(d):
@@ -31,178 +33,160 @@ def suffix(d):
 def custom_strftime(format, t):
     return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
 
+# Set date one week from now
 nowDate = datetime.datetime.now()
-oneWeeks = datetime.timedelta(weeks = 1)
-week = nowDate + oneWeeks
+week = datetime.timedelta(weeks = 1)
 
-oneWeekFromNow = custom_strftime('%A, %B {S}, %Y', dt.now() + oneWeeks)
-print "oneWeekFromNow is: " + oneWeekFromNow
-oneWeekFromNowSplit = oneWeekFromNow.split(',')
-oneWeekFromNowDay = oneWeekFromNowSplit[1]
-print oneWeekFromNowDay
-oneWeekFromNowDayCleaned = (re.sub(r'\D+$', '', oneWeekFromNowDay))
-oneWeekFromNowDayFinal = (oneWeekFromNowSplit[0] + "," + oneWeekFromNowDayCleaned + "," + oneWeekFromNowSplit[2])
-print oneWeekFromNowDayFinal
-oneWeekFromNowDayFinalDateTime = datetime.datetime.strptime(oneWeekFromNowDayFinal, '%A, %b %d, %Y')
-print oneWeekFromNowDayFinalDateTime
+oneWeek = custom_strftime('%A, %B {S}, %Y', dt.now() + week)
+oneWeekSplit = oneWeek.split(',')
+oneWeekDay = oneWeekSplit[1]
+oneWeekDayCleaned = (re.sub(r'\D+$', '', oneWeekDay))
+oneWeekDayFinal = (oneWeekSplit[0] + "," + oneWeekDayCleaned + "," + oneWeekSplit[2])
+#p=datetime.datetime.strptime('June 5, 2019', '%b %d, %Y')
+#print p
+oneWeekDateTime = datetime.datetime.strptime(oneWeekDayFinal, '%A, %B %d, %Y')
+print oneWeekDateTime
+
 #print "oneWeekFromNow Split is: "+ oneWeekFromNowSplit[1]
 
 #oneWeekFromToday = todaysDate + oneWeek
 
 #print "oneWeekFromToday is: " + oneWeekFromToday
 
-now = datetime.datetime.now()
-print (now.strftime("%A, %b %d, %Y"))
+#now = datetime.datetime.now()
+#print (now.strftime("%A, %b %d, %Y"))
 
 
-oneWeek = datetime.timedelta(weeks = 1)
+#oneWeek = datetime.timedelta(weeks = 1)
 
-weekFromToday = now + oneWeek
-print (weekFromToday.strftime("%A, %b %d, %Y"))
-weekFromTodayString = custom_strftime('%A, %B {S}, %Y', dt.now())
+#weekFromToday = now + oneWeek
+#print (weekFromToday.strftime("%A, %b %d, %Y"))
+#weekFromTodayString = custom_strftime('%A, %B {S}, %Y', dt.now())
 
 
-testVariable = "Monday, May 13, 2019"
-testSplit = testVariable.split(',')
-print testSplit
-print testSplit[1]
 #Set up geocoder
 geocoder = mapbox.Geocoder(access_token='pk.eyJ1Ijoic3RhcnJtb3NzMSIsImEiOiJjam13ZHlxbXgwdncwM3FvMnJjeGVubjI5In0.-ridMV6bkkyNhbPfMJhVzw')
-
 
 #Set up web driver and base URL
 driver= webdriver.Chrome(executable_path='/Applications/chromedriver74')
 
+#Set base url
 base_url = 'https://www.bandsintown.com/?came_from=257&page='
-#this page gives 404 error!
-#https://www.bandsintown.com/e/1009883476-kronos-quartet-and-mahsa-vahdat-at-weill-hall?came_from=257&utm_medium=web&utm_source=home&utm_campaign=event
+
 events = []
 eventContainerBucket = []
+
 for i in range(1, 50):
-    #pageSource = driver.page_source
-    #print pageSource + "page source"
-    #if not driver.page_source().contains("404"):
-    #and
-    #if not driver.getPageSource().contains("not found"):
+
+    #cycle through pages in range
     driver.get(base_url + str(i))
     pageURL = base_url + str(i)
-    #print pageURL
-    #r = requests.get(pageURL)
-    #print(r.status_code)
-    #r = requests.get(pageURL)
-    #print (r.status_code)
 
     # get events links
     event_list = driver.find_elements_by_css_selector('div[class^=eventList-] a[class^=event-]')
     # collect href attribute of events in even_list
     events.extend(list(event.get_attribute("href") for event in event_list))
 
-print(len(events))
+print "total events: ", (len(events))
 
-# iterate throw all events and open them.
+# iterate through all events and open them.
 item = {}
 for event in events:
 
     driver.get(event)
     currentUrl = driver.current_url
     currentRequest = requests.get(currentUrl)
-    print currentRequest.status_code
+    #print currentRequest.status_code
 
     if currentRequest.status_code == 200:
-        print ("link working")
+        #print ("link working")
 
-        #print event
-        #uniqueEventContainer = driver.find_elements_by_css_selector('div[class^=eventInfoContainer-]')[0]
         soup = bs(driver.find_element_by_css_selector('[class^=eventInfoContainer-]').get_attribute('outerHTML'))
         soup2 = bs(driver.find_element_by_css_selector('[class^=artistAndEventInfo-]').get_attribute('outerHTML'))
         containers = driver.find_elements_by_css_selector('div[class^=eventInfoContainer-]')
         date_time = containers[1].text.split('\n')
 
+        # Only pull one weeks worth of data
+        # Find date of event and format to compare against one week from now date
         dateMatch = soup.select_one('img + div').text
-        datetime1 = datetime.datetime.strptime(testVariable, '%A, %b %d, %Y')
-        print datetime1
-        #oneWeekFromNowDate = datetime.datetime.strptime(re.sub('th|nd|st', '', oneWeekFromNow), '%A, %b %d, %Y')
-        #dateMatchDate = datetime.datetime.strptime(re.sub('th|nd|st', '', dateMatch), '%A, %b %d, %Y')
-
-        #New approach
-
         dateMatchSplit = dateMatch.split(',')
         dateMatchDay = dateMatchSplit[1]
-
         dateMatchCleaned = (re.sub(r'\D+$', '', dateMatchDay))
         dateMatchDayFinal = (
                 dateMatchSplit[0] + "," + dateMatchCleaned + "," + dateMatchSplit[2])
-        print dateMatchDayFinal
-        dateMatchDayFinalDate = datetime.datetime.strptime(dateMatchDayFinal, '%A, %b %d, %Y')
-        #one = datetime.datetime.strptime(dateMatchDayFinal, '%A, %b %d, %Y')
-        print "comparing now!"
-        print dateMatchDayFinalDate, oneWeekFromNowDayFinalDateTime
+        dateMatchDate = datetime.datetime.strptime(dateMatchDayFinal, '%A, %B %d, %Y')
+        eventDate = str(dateMatchDate).replace("00:00:00","")
+        eventDate = eventDate.rstrip()
 
-        if dateMatchDayFinalDate <= oneWeekFromNowDayFinalDateTime:
-            print "match!"
+        #print dateMatchDate, oneWeekDateTime
 
+        #compare date of event to one week from now date
+        if dateMatchDate <= oneWeekDateTime:
+            print "this event occurs one week or less from today"
 
-            #datetime2 = datetime.datetime.strptime(oneWeekFromNow, '%A, %B {S}, %Y')
-            #print (datetime.datetime.strptime(dateMatch, '%A, %b %d, %Y'))
-
-            #print soup
-            #print soup2
+            # Get artist
             artist = soup2.select_one('h1').text
-            #print artist
-            #print artist
+
+            # Get date
             date = soup.select_one('img + div').text
+
+            # Get time
             time = soup.select_one('img + div + div').text
-            #thing = soup.select_one('img + div + div + img').text
+
+            # Get address
             address = soup.select_one('[class^=eventInfoContainer-2d9f07df]:nth-of-type(1) div + div').text
 
+            # Get venue
             venue = soup.select_one('[class^=eventInfoContainer-2d9f07df]:nth-of-type(1) div').text
-            geocodeInput = venue + ", " + address
-            print geocodeInput
+
+            #Get image
+            artistImage = soup.select_one('img')
+            #artistImage.get_attribute("src")
+            #print "img is", artistImage
+            #x=driver.find_element_by_css_selector('img')
+            #print x
+
+            #image = soup.select_one('[class^=artistAndEventInfo-7c13900b] img')
+            #print "band image", image
+            #container = soup.find('div', {"class": "artistAndEventInfo-7c13900b"})
+            #children = container.find("img")
+            #thing = driver.find_element_by_xpath("//div[@class^=artistAndEventInfo-7c13900b']/img")
+            #print thing
+            print(driver.find_element_by_xpath("//div[@class='artistAndEventInfo-7c13900b']//img").get_attribute("src"))
+
+            #Get other event information
+            #otherInfo = soup.select_one('[class^=eventInfoContainer-a1c6de30]').text
+            #print "other info", otherInfo
 
             #Geocode address
+            geocodeInput = venue + ", " + address
             response = geocoder.forward(address)
             result = response.json()
 
-            #Bin into 'item'
+            #Bin information into 'item'
             item['Artist'] = artist
             item['Date'] = date
+            item['eventDate']  = eventDate
             item['Time'] = time
             item['Venue'] = venue
             item['Address'] = address
 
             print ("date is:" + date)
+            print ("datematch date is", dateMatchDate)
 
-
-            # address = soup.select_one('[class^=eventInfoContainer-]:nth-of-type(3) div + div').text
-            # print(date, time, address)
-            #locationImg = soup.select_one('[src="https://assets.bandsintown.com/images/pin.svg"]')['src']
-            #t = locationImg.find_next_sibling("div")
-            #thing = driver.find_element_by_css_selector("img[src='https://assets.bandsintown.com/images/pin.svg']") #this works
-            #venue_address = place.text.split('\n')
-            #venue = venue_address[0]
-            #address = venue_address[1]
-
-            #print date, time, venue, address
-            #item[event] = item
-            #print result['features'][1]['center']
+            # Get latitude, longitude
             coordinates = result['features'][0]['center']
             item['Coordinates'] = coordinates
 
             print result
             print address, str(coordinates)
-            #print "Geocode input, Coords are: " + address, coordinates
-            #print ["%0.6f" % i for i in coordinates]
-            #print item
-            #for element in result['features']:
-                #coordinates = element['center']
-                #
-                #print address, coordinates
 
-            case = {'Artist': item['Artist'], 'Date': item['Date'], 'Time': item['Time'], 'Venue': item['Venue'],
+            # Format output to JSON
+            case = {'Artist': item['Artist'], 'Date': item['Date'], 'EventDate': item['eventDate'], 'Time': item['Time'], 'Venue': item['Venue'],
             'Address': item['Address'], 'Coordinates': item['Coordinates']}
             item[event] = case
 
-            print item
+            #print item
 
 with open("testScrape.json", "w") as writeJSON:
    json.dump(item, writeJSON)
@@ -210,4 +194,5 @@ with open("testScrape.json", "w") as writeJSON:
     #print (uniqueDate)
 
 
-print result
+#print result
+print "Data pull complete!"
